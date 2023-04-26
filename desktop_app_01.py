0012b7a396f4c253b3df7fcb26dd133761c6922e
fsadriann <adrianforero@colegioguanenta.edu.co>
2023-04-26 08:58:54 -0500

    se hizo el programa

---
+++ desktop_app_01.py
@@ -0,0 +1,22 @@
+#------------------------
+#    Desktop app No.1
+#------------------------
+
+#se importa la libreria tkinter con todas sus funciones
+from tkinter import *
+
+#------------------------
+#   funciones de la app
+#------------------------
+
+#------------------------------
+# ventana principal de la app
+#------------------------------
+
+
+#se declara una variable llamada ventana_principal, que adquiere las propiedades de un objeto Tk
+ventana_principal = Tk()
+
+
+#run
+ventana_principal.mainloop()
