b3dfcf566f90bf165c8dd9f9d5a56135d3384413
fsadriann <adrianforero@colegioguanenta.edu.co>
2023-05-04 15:34:49 -0500

    se terminó el programa

--- suma_enteros.py
+++ suma_enteros.py
@@ -10,9 +10,9 @@ from tkinter import messagebox
 #   funciones de la app
 #------------------------
 
-# sumar
+#sumar
 def calcular():
-    messagebox.showinfo("Mini Calculadora 1.0", "las operaciones serán realizadas")
+    messagebox.showinfo("minicalculadora 1.0", "las operaciones han sido realizadas")
     s = int(x.get()) + int(y.get())
     r = int(x.get()) - int(y.get())
     m = int(x.get()) * int(y.get())
@@ -27,56 +27,54 @@ def calcular():
     t_resultados.insert(INSERT, f"\n{int(x.get())} // {int(y.get())} = {de}")
     t_resultados.insert(INSERT, f"\n{int(x.get())} % {int(y.get())} = {mod}")
     t_resultados.insert(INSERT, f"\n{int(x.get())} ** {int(y.get())} = {p}")
-
-
-# borrar
+    
+    
+    
+#borrar
 def borrar():
-    messagebox.showinfo("Mini Calculadora 1.0", "los datos serán borrados")
+    messagebox.showinfo("minicalculadora 1.0", "los datosseran borrados")
     x.set("")
     y.set("")
     t_resultados.delete("1.0", "end")
+    
 
-# salir
+#salir
 def salir():
-    messagebox.showinfo("Mini Calculadora 1.0", "La app se cerrará")
+    messagebox.showinfo("mini calculadora 1.0", "la app se va a cerrar")
     ventana_principal.destroy()
 
-#------------------------------
-# ventana principal de la app
-#------------------------------
+
+#Ventana principal de la app
 
 
-# se declara una variable llamada ventana_principal, que adquiere las propiedades de un objeto Tk
+#Se declara una ventana llamada ventana_principal, que adquiere las caracteristicas de un objeto TK()
 ventana_principal = Tk()
 
-# titulo de la ventana
+#titulo de la ventana
+ventana_principal.title("minicalculadora 1.0")
 
-ventana_principal.title("Mini calculadora 1.0")
-
-# tamaño de la ventana
-
+#Tamaño de la ventana
 ventana_principal.geometry("500x500")
 
-# deshabilitar botón de maximizar
-
+#Desabilitar el boton de maximizar
 ventana_principal.resizable(False, False)
 
-# color de fondo de la ventana
-
+#Color de fondo de la ventana
 ventana_principal.config(bg="navy")
 
-#------------------------
+
+#---------------------------
 # variables globales
-#------------------------
+#---------------------------
 
-x=StringVar()
-y=StringVar()
+x = StringVar()
+y = StringVar()
 
-#------------------------
-# frame entrada datos
-#------------------------
+#-----------------------------------
+# Entrada datos
+#-----------------------------------
 frame_entrada = Frame(ventana_principal)
-frame_entrada = Frame(bg="white", width=480, height=180)
+frame_entrada.config(bg="white", width=480, height=180)
 frame_entrada.place(x=10, y=10)
 
 # logo de la app
@@ -89,33 +87,36 @@ titulo=Label(frame_entrada, text="Mini calculadora 1.0")
 titulo.config(font=("Arial", 16), bg="white", fg="navy")
 titulo.place(x=240, y=10)
 
-# etiqueta para el valor de x
-lb_x = Label(frame_entrada, text="X = ")
-lb_x.config(font=("Arial", 16), bg="white", fg="navy")
+#etiqueta para el valor de x
+lb_x=Label(frame_entrada, text= "X =")
+lb_x.config(bg="white", fg="navy", font=("helvetica", 20))
 lb_x.place(x=240, y=60)
 
-# caja de texto para x
-Entry_x = Entry(frame_entrada, textvariable=x)
-Entry_x.config(font=("Arial", 16), bg="white", fg="navy",)
-Entry_x.focus_set()
-Entry_x.place(x=280, y=60, width=100)
+#caja de texto para el valor x
+entry_x=Entry(frame_entrada, textvariable=x)
+entry_x.config(bg="white", fg="navy", font=("Helvetica", 18),width=6)
+entry_x.focus_set()
+entry_x.place(x=290, y=60)
 
-# etiqueta para el valor de y
-lb_y = Label(frame_entrada, text="Y = ", textvariable=y)
-lb_y.config(font=("Arial", 16), bg="white", fg="navy")
-lb_y.place(x=240, y=100)
+#etiqueta para el valor de y
+lb_y=Label(frame_entrada, text= "Y =")
+lb_y.config(bg="white", fg="navy", font=("helvetica", 20))
+lb_y.place(x=240, y=120)
 
-# caja de texto para y
-Entry_y = Entry(frame_entrada)
-Entry_y.config(font=("Arial", 16), bg="white", fg="navy")
-Entry_y.place(x=280, y=100, width=100)
+#caja de texto para el valor y
+entry_y=Entry(frame_entrada, textvariable=y)
+entry_y.config(bg="white", fg="navy", font=("helvetica", 18),width=6)
+entry_y.focus_set()
+entry_y.place(x=290, y=120)
 
-#------------------------
-# frame operaciones
-#------------------------
+#----------------------------------
+# frama operaciones
+#-----------------------------------
+
 frame_operaciones = Frame(ventana_principal)
-frame_operaciones = Frame(bg="white", width=480, height=100)
+frame_operaciones.config(bg="white", width=480, height=100)
 frame_operaciones.place(x=10, y=200)
+
 
 # boton para calcular
 bt_sumar = Button(frame_operaciones, text="Calcular", command=calcular)
@@ -132,16 +133,16 @@ bt_salir = Button(frame_operaciones, text="Salir", command=salir)
 bt_salir.config(font=("Arial", 16), bg="white", fg="navy")
 bt_salir.place(x=335, y=35, width=100, height=30)
 
-#------------------------
+#-----------------------------------
 # frame resultados
-#------------------------
+#-----------------------------------
 frame_resultados = Frame(ventana_principal)
-frame_resultados = Frame(bg="white", width=480, height=180)
+frame_resultados.config(bg="white", width=480, height=180)
 frame_resultados.place(x=10, y=310)
 
-# area de texto para resultados
-t_resultados = Text(frame_resultados)
-t_resultados.config(font=("Courier", 20), bg="black", fg="green yellow")
+#area de texto para los resultados
+t_resultados=Text(frame_resultados)
+t_resultados.config(bg="black", fg="green yellow",font=("courier",18))
 t_resultados.place(x=10, y=10, width=460, height=160)
 
 # run
